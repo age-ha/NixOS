1ed9bc551fb982a2e5ace99f96e2bb2e5bc3cdd0
age-ha <125471884+age-ha@users.noreply.github.com>
2024-01-04 19:03:18 +0800

    Add files via upload

---
+++ home-manager/home.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{ 
+  home.username = "ageha";
+  home.homeDirectory = "/home/ageha";
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 22;
+  };
+  home.packages = with pkgs;[
+  python39
+  ];
+  home.stateVersion = "24.05";
+  #programs.home-manager.enable = true;
+}
